d17846d9bd8506d8a9d5b5a1c83116b1497e8a95
Ahmed <saberahmed690@gmail.com>
2020-03-05 19:05:01 +0200

    Ahmed&Moamen

---
+++ app/src/main/java/com/ProjectITI/tripsproject/AddTrip.java
@@ -0,0 +1,174 @@
+package com.ProjectITI.tripsproject;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import android.app.AlarmManager;
+import android.app.DatePickerDialog;
+import android.app.PendingIntent;
+import android.app.TimePickerDialog;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.DatePicker;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.TimePicker;
+import android.widget.Toast;
+
+import java.text.DateFormat;
+import java.util.Calendar;
+
+import com.ProjectITI.tripsproject.AlertReceiver;
+import com.example.tripsproject.TimePickerFragment;
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+
+import java.util.Arrays;
+
+public class AddTrip extends AppCompatActivity implements TimePickerDialog.OnTimeSetListener{
+
+    private TextView timeSelected ;
+    private ImageView alarmPicker ;
+    DatePickerDialog picker;
+    private TextView dateSelected ;
+    private ImageView datePicker ;
+
+    PlacesClient placesClient;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_trip);
+        timeSelected = findViewById(R.id.time_Selected) ;
+        alarmPicker = findViewById(R.id.alarm_Picker) ;
+        alarmPicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DialogFragment timePicker = new TimePickerFragment();
+                timePicker.show(getSupportFragmentManager(), "time picker");
+            }
+        });
+        dateSelected = findViewById(R.id.date_Selected) ;
+        datePicker = findViewById(R.id.date_Picker) ;
+        datePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                picker = new DatePickerDialog(AddTrip.this,new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                dateSelected.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
+
+        String apiKey = "AIzaSyCsCJEIvV6hUuaAb4R5wVJIPuikZ9TN5ag";
+
+        // Setup Places Client
+        if (!Places.isInitialized()) {
+            Places.initialize(AddTrip.this, apiKey);
+        }
+        // Retrieve a PlacesClient (previously initialized - see MainActivity)
+        placesClient = Places.createClient(this);
+
+        //StartPoint Fragment
+
+        final AutocompleteSupportFragment autocompleteStartPointFragment =
+                (AutocompleteSupportFragment)
+                        getSupportFragmentManager().findFragmentById(R.id.startPoint_fragment);
+
+        autocompleteStartPointFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME,Place.Field.LAT_LNG,Place.Field.ADDRESS));
+
+        autocompleteStartPointFragment.setOnPlaceSelectedListener(
+                new PlaceSelectionListener() {
+                    @Override
+                    public void onPlaceSelected(Place place) {
+                        final LatLng latLng = place.getLatLng();
+
+                        Toast.makeText(AddTrip.this, ""+latLng.latitude, Toast.LENGTH_SHORT).show();
+
+                    }
+
+                    @Override
+                    public void onError(Status status) {
+                        Toast.makeText(AddTrip.this, ""+status.getStatusMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+        //EndPoint Fragment
+        final AutocompleteSupportFragment autocompleteEndPointFragment =
+                (AutocompleteSupportFragment)
+                        getSupportFragmentManager().findFragmentById(R.id.endPoint_fragment);
+
+        autocompleteEndPointFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME,Place.Field.LAT_LNG,Place.Field.ADDRESS));
+
+        autocompleteEndPointFragment.setOnPlaceSelectedListener(
+                new PlaceSelectionListener() {
+                    @Override
+                    public void onPlaceSelected(Place place) {
+                        final LatLng latLng = place.getLatLng();
+
+                        Toast.makeText(AddTrip.this, ""+latLng.latitude, Toast.LENGTH_SHORT).show();
+
+                    }
+
+                    @Override
+                    public void onError(Status status) {
+                        Toast.makeText(AddTrip.this, ""+status.getStatusMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+
+    @Override
+    public void onTimeSet(TimePicker timePicker, int hourOfDay, int minute) {
+        Calendar c = Calendar.getInstance();
+        c.set(Calendar.HOUR_OF_DAY, hourOfDay);
+        c.set(Calendar.MINUTE, minute);
+        c.set(Calendar.SECOND, 0);
+
+        updateTimeText(c);
+        startAlarm(c);
+    }
+    private void updateTimeText(Calendar c) {
+        String timeText = "";
+        timeText += DateFormat.getTimeInstance(DateFormat.SHORT).format(c.getTime());
+
+        timeSelected.setText(timeText);
+    }
+
+    private void startAlarm(Calendar c) {
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(this, AlertReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, 0);
+
+        if (c.before(Calendar.getInstance())) {
+            c.add(Calendar.DATE, 1);
+        }
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            alarmManager.setExact(AlarmManager.RTC_WAKEUP, c.getTimeInMillis(), pendingIntent);
+        }
+    }
+//    private void cancelAlarm() {
+//        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+//        Intent intent = new Intent(this, AlertReceiver.class);
+//        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1, intent, 0);
+//
+//        alarmManager.cancel(pendingIntent);
+//        timeSelected.setText("Alarm canceled");
+//    }
+
+}
